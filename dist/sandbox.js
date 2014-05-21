41ea5f4405316a3f6519d5360480b84464bb4426
Josh Marchán <sykopomp@sykosomatic.org>
2014-05-21 01:29:14 -0700

    Working drag and drop example

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -40,6 +40,118 @@ var list2 = list1.bind().toCanList(new can.List([1,2,3]));
 list1.bind().log("list1 changed");
 list2.bind().log("list2 changed");
 
+function domStream(name) {
+  return function(target) {
+    return can.$(target||document).asEventStream(name);
+  };
+}
+var Mouse = {
+  mousemove: domStream("mousemove"),
+  mouseup: domStream("mouseup"),
+  mousedown: domStream("mousedown"),
+  click: domStream("click"),
+  position: function(target) {
+    return Mouse.mousemove(target)
+      .map(function(ev) {return [ev.pageX, ev.pageY];})
+      .toProperty();
+  },
+  isUp: function(target) {
+    return Mouse.mouseup(target).map(true)
+      .merge(Mouse.mousedown(target).map(false))
+      .toProperty();
+  },
+  isDown: function(target) {
+    return Mouse.isUp(target).not();
+  }
+};
+
+var Window = {
+  height: function() {
+    return Window.dimensions().map(".0");
+  },
+  width: function() {
+    return Window.dimensions().map(".1");
+  },
+  dimensions: function() {
+    var win = can.$(window);
+    return win.asEventStream("resize")
+      .map(function(){ return [win.outerWidth(), win.outerHeight()]; })
+      .toProperty([win.outerWidth(), win.outerHeight()]);
+  },
+  animationFrames: function() {
+    return Bacon.fromBinder(function(sink) {
+      var done = false,
+          requestID;
+      function request() {
+        requestID = window.requestAnimationFrame(function(x) {
+          sink(x);
+          if (!done) {request();}
+        });
+      }
+      request();
+      return function stop() {
+        done = true;
+        if (requestID) {
+          window.cancelAnimationFrame(requestID);
+        }
+      };
+    });
+  }
+};
+
+can.Component.extend({
+  tag: "drag-drop-demo",
+  template: can.view("#drag-drop-demo-tpl"),
+  scope: {
+    clamp: true,
+    boxPosition: {x: 0, y: 0}
+  },
+  events: {
+    clampToDemo: function(box, coordinates) {
+      var el = this.element;
+      return [clamp(coordinates[0], 0, el.width()-box.width()),
+              clamp(coordinates[1], 0, el.height()-box.height())];
+    },
+    mouseDeltas: function() {
+      return Mouse.position().diff(null, function(a, b) {
+        return a ? [b[0] - a[0], b[1] - a[1]] : [0,0];
+      });
+    },
+    boxPosition: function(box) {
+      var startPos = box.position();
+      return this.mouseDeltas()
+        .scan([startPos.left, startPos.top], function(a, b) {
+          return [a[0]+b[0], a[1]+b[1]];
+        }).map(this.scope.attr("clamp") ?
+               this.clampToDemo.bind(this, box) :
+               function(x){return x;});
+    },
+    ".draggable-box mousedown": function(box, ev) {
+      var scope = this.scope,
+          el = this.element,
+          boxPosition = this.boxPosition(box)
+            .sampledBy(Window.animationFrames())
+            .takeWhile(Mouse.isDown());
+      can.$("body").addClass("drag-drop-demo-dragging");
+      el.trigger("dragstart");
+      boxPosition.onEnd(function() {
+        can.$("body").removeClass("drag-drop-demo-dragging");
+        el.trigger("dragend");
+      });
+      boxPosition.onValue(function(coords) {
+        scope.attr("boxPosition", {
+          x: coords[0],
+          y: coords[1]
+        });
+      });
+    }
+  }
+});
+
+function clamp(num, min, max) {
+  return Math.max(min, Math.min(num, max));
+}
+
 $("#container").html(can.view("#container-template", {
   compute1: compute1,
   compute2: compute2,
