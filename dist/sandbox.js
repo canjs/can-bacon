e5620d772c46694aacc132116cd500c48a39381b
Josh Marchán <sykopomp@sykosomatic.org>
2014-05-29 19:07:21 -0700

    Playing around with sandbox

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -54,69 +54,11 @@ $("#container").html(can.view("#container-template", {
 }));
 
 /**
- * Drag and drop component
- */
-can.Component.extend({
-  tag: "drag-drop-demo",
-  template: can.view("#drag-drop-demo-tpl"),
-  scope: {
-    clamp: true,
-    boxPosition: {x: 0, y: 0}
-  },
-  events: {
-    clampToDemo: function(box, coordinates) {
-      var el = this.element;
-      return [clamp(coordinates[0], 0, el.width()-box.width()),
-              clamp(coordinates[1], 0, el.height()-box.height())];
-    },
-    mouseDeltas: function() {
-      return Mouse.position().diff(null, function(a, b) {
-        return a ? [b[0] - a[0], b[1] - a[1]] : [0,0];
-      });
-    },
-    boxPosition: function(box) {
-      var startPos = box.position();
-      return this.mouseDeltas()
-        .scan([startPos.left, startPos.top], function(a, b) {
-          return [a[0]+b[0], a[1]+b[1]];
-        }).map(this.scope.attr("clamp") ?
-               this.clampToDemo.bind(this, box) :
-               function(x){return x;});
-    },
-    ".draggable-box mousedown": function(box, ev) {
-      var scope = this.scope,
-          el = this.element,
-          boxPosition = this.boxPosition(box)
-            .sampledBy(Window.animationFrames())
-            .takeWhile(Mouse.isDown());
-      can.$("body").addClass("drag-drop-demo-dragging");
-      el.trigger("dragstart");
-      boxPosition.onEnd(function() {
-        can.$("body").removeClass("drag-drop-demo-dragging");
-        el.trigger("dragend");
-      });
-      boxPosition.onValue(function(coords) {
-        scope.attr("boxPosition", {
-          x: coords[0],
-          y: coords[1]
-        });
-      });
-    }
-  }
-});
-
-function clamp(num, min, max) {
-  return Math.max(min, Math.min(num, max));
-}
-
-$("#container").append(can.stache("<drag-drop-demo></drag-drop-demo>")());
-
-/**
  * DOM Utility "modules"
  */
 function domStream(name) {
   return function(target) {
-    return can.$(target||document).asEventStream(name);
+    return $(target||document).asEventStream(name);
   };
 }
 var Mouse = {
@@ -124,33 +66,52 @@ var Mouse = {
   mouseup: domStream("mouseup"),
   mousedown: domStream("mousedown"),
   click: domStream("click"),
+  clicks: function(target) {
+    return Mouse.click(target).map(function(ev) {
+      return {
+        x: target ? ev.offsetX : ev.pageX,
+        y: target ? ev.offsetY : ev.pageY
+      };
+    });
+  },
+  deltas: function(target) {
+    return Mouse.position(target).diff(null, function(a, b) {
+      return a ? {x: b.x - a.x, y: b.y - a.y} : {x: 0, y: 0};
+    }).toEventStream();
+  },
   position: function(target) {
     return Mouse.mousemove(target)
-      .map(function(ev) {return [ev.pageX, ev.pageY];})
+      .map(function(ev) {
+        return {
+          x: target ? ev.offsetX : ev.pageX,
+          y: target ? ev.offsetY : ev.pageY
+        };
+      }).toProperty();
+  },
+  isUp: function(upTarget, downTarget) {
+    return Mouse.mouseup(upTarget).map(true)
+      .merge(Mouse.mousedown(downTarget).map(false))
       .toProperty();
   },
-  isUp: function(target) {
-    return Mouse.mouseup(target).map(true)
-      .merge(Mouse.mousedown(target).map(false))
-      .toProperty();
-  },
-  isDown: function(target) {
-    return Mouse.isUp(target).not();
+  isDown: function(downTarget, upTarget) {
+    return Mouse.isUp(upTarget, downTarget).not();
   }
 };
 
 var Window = {
   height: function() {
-    return Window.dimensions().map(".0");
+    return Window.dimensions().map(".height");
   },
   width: function() {
-    return Window.dimensions().map(".1");
+    return Window.dimensions().map(".width");
   },
   dimensions: function() {
-    var win = can.$(window);
+    var win = $(window);
     return win.asEventStream("resize")
-      .map(function(){ return [win.outerWidth(), win.outerHeight()]; })
-      .toProperty([win.outerWidth(), win.outerHeight()]);
+      .map(function(){
+        return {width: win.outerWidth(), height: win.outerHeight()};
+      })
+      .toProperty({width: win.outerWidth(), height: win.outerHeight()});
   },
   animationFrames: function() {
     return Bacon.fromBinder(function(sink) {
@@ -172,3 +133,52 @@ var Window = {
     });
   }
 };
+
+/**
+ * Drag and drop component
+ */
+can.Component.extend({
+  tag: "drag-drop-demo",
+  template: can.view("#drag-drop-demo-tpl"),
+  scope: {
+    clamp: true,
+    boxPosition: {x: 0, y: 0}
+  },
+  events: {
+    clampToDemo: function(box, coordinates) {
+      var el = this.element;
+      return {
+        x: clamp(coordinates.x, 0, el.width()-box.width()),
+        y: clamp(coordinates.y, 0, el.height()-box.height())
+      };
+    },
+    boxPosition: function(box) {
+      var startPos = box.position();
+      return Mouse.deltas()
+        .scan({x: startPos.left, y: startPos.top}, function(a, b) {
+          return {x: a.x + b.x, y: a.y + b.y};
+        }).map(this.scope.attr("clamp") ?
+               this.clampToDemo.bind(this, box) :
+               function(x){return x;});
+    },
+    inserted: function() {
+      var scope = this.scope,
+          el = this.element,
+          control = this,
+          box = el.children(".draggable-box"),
+          boxHeld = Mouse.isDown(box);
+      boxHeld.assign(can.$("body"), "toggleClass", "drag-drop-demo-dragging");
+      boxHeld.changes().filter(function(x){return x;}).onValue(function() {
+        control.boxPosition(box)
+          .takeWhile(boxHeld)
+          .assign(scope, "attr", "boxPosition");
+      });
+    }
+  }
+});
+
+function clamp(num, min, max) {
+  return Math.max(min, Math.min(num, max));
+}
+
+$("#container").append(can.stache("<drag-drop-demo></drag-drop-demo>")());
