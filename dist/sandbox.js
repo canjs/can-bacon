eb34c72ce1c9a920efa178b86bdfeebace0200d7
Kat Marchán <kzm@sykosomatic.org>
2014-09-17 14:00:30 -0700

    Use new API in sandbox, too

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -6,7 +6,7 @@
  */
 var compute1 = can.compute(1),
     property1 = compute1.bind(),
-    compute2 = property1.toCanCompute(),
+    compute2 = property1.toCompute(),
     property2 = compute2.bind(),
     recentLimit = 5,
     recentChanges = property1.scan({count:0}, function(acc, val) {
@@ -33,24 +33,24 @@ recentChanges.log("recentChanges");
 total.log("total");
 
 var map1 = new can.Map({a:1,b:2});
-var map2 = map1.bind().toCanMap();
+var map2 = map1.bind().toMap();
 // Super-magical happy fun time two-way binding between maps :)
-map2.bind().toCanMap(map1);
+map2.bind().toMap(map1);
 map1.bind().log("map1 changed");
 map2.bind().log("map2 changed");
 
 var list1 = new can.List([1,2,3]);
-var list2 = list1.bind().toCanList(new can.List([1,2,3]));
+var list2 = list1.bind().toList(new can.List([1,2,3]));
 // This one doesn't work quite as well as one would hope.
-// list2.bind().toCanList(list1);
+// list2.bind().toList(list1);
 list1.bind().log("list1 changed");
 list2.bind().log("list2 changed");
 
 $("#container").html(can.view("#container-template", {
   compute1: compute1,
   compute2: compute2,
-  recent: recentChanges.toCanList(),
-  total: total.toCanCompute()
+  recent: recentChanges.toList(),
+  total: total.toCompute()
 }));
 
 /**
